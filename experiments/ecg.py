1a0b235d4709084f4f0836dc7a3133313f2e3484
katie <xuke0531@hotmail.com>
2026-06-16 10:28:58 +0800

    所有结果

--- experiments/ecg.py
+++ experiments/ecg.py
@@ -426,7 +426,7 @@ if __name__ == "__main__":
     #     )
     # else:
     #     raise ValueError(f"{args.name} is not a valid experiment name")
-    train_ecg_model(args.latent_dim, args.batch_size, model_name=model_name)
-    concept_accuracy(args.seeds, args.latent_dim, args.plot, model_name=model_name)
-    statistical_significance(args.seeds[0], args.latent_dim, model_name=model_name)
+    # train_ecg_model(args.latent_dim, args.batch_size, model_name=model_name)
+    # concept_accuracy(args.seeds, args.latent_dim, args.plot, model_name=model_name)
+    # statistical_significance(args.seeds[0], args.latent_dim, model_name=model_name)
     feature_importance(args.seeds[0],args.batch_size,args.latent_dim,args.plot,model_name=model_name,)
